14a8b89293f12cb7259de327137b905d6a91b9c7
Edwin <preteledwin23@gmail.comcleargit config --global user.email preteledwin23>
2023-10-25 18:11:39 -0500

    Initial commit

---
+++ login_history.sh
@@ -0,0 +1,19 @@
+# Script Name:                  Variables 101
+# Author:                       Edwin Pretel
+# Date of latest revision:      10/25/2023
+# Purpose:                      functions
+
+
+
+
+
+
+#!/bin/bash
+
+print_login_history() {
+    echo "This is the login history:"
+    grep "Accepted password" /var/log/auth.log
+}
+
+# Call the function
+print_login_history
