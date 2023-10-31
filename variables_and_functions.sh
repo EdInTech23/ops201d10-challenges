d3e72300a28985622902bcac5f4709ff5f63b448
Edwin <preteledwin23@gmail.comcleargit config --global user.email preteledwin23>
2023-10-30 22:00:18 -0500

    vriables

---
+++ variables_and_functions.sh
@@ -0,0 +1,35 @@
+
+
+# Declaration of variables
+
+# Price of apples
+# ONE +SIGN MEANS ASSIGNING A VALUE
+# TWO IS COMPARING VALUES
+
+PRICE_PER_APPLE=5
+myfirstletters=abc
+greeting='hello world'
+files=`ls`
+who=`whoami`
+
+# Declaration of functions
+# Basic function that handles printing the greeting to the screen
+# function is reusibale piece of code
+print_greeting() {
+    echo 'hello world'
+    echo 'this is our first function'
+}
+
+
+
+# call the function - tell the computer to do the thing
+print_greeting
+
+
+# Main
+#echo "the price of an apple today is: $PRICE_PER_APPLE"
+#echo $greeting
+#echo "The first ten letter of the alphabet are: ${myfirstletters}defghi"
+#echo "This folder contains the following $files"
+#echo "theperson running this scrips is $who"
+# End
